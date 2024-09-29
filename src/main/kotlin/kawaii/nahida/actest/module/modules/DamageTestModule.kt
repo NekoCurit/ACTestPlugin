32ebc18ab5bdb15e45920717585406a094cb0ec3
NekoCurit <108512490+NahidaLing@users.noreply.github.com>
2024-09-29 21:22:23 +0800

    refactor(Module\DamageTest): include cancelled

--- src/main/kotlin/kawaii/nahida/actest/module/modules/DamageTestModule.kt
+++ src/main/kotlin/kawaii/nahida/actest/module/modules/DamageTestModule.kt
@@ -1,4 +1,5 @@
 package kawaii.nahida.actest.module.modules
+import kawaii.nahida.actest.ACTest
 import kawaii.nahida.actest.module.Module
 import kawaii.nahida.actest.utils.bukkit.PlayerExtensions.sendActionBar
 import org.bukkit.entity.Player
@@ -10,17 +11,18 @@ class DamageTestModule : Module("DamageTest") {
 
     @EventHandler
     fun onPlayerAttack(event: EntityDamageByEntityEvent) {
-        if (event.isCancelled) return
-
         (event.damager as? Player)
-            ?.sendActionBar(
-                arrayOf(
-                    "预测伤害: ${String.format("%.1f", event.damage)}",
-                    "实际伤害: ${String.format("%.1f", event.finalDamage)}",
-                    "预测距离: ${String.format("%.2f", event.damager.location.distance(event.entity.location))}",
-                    "攻击疾跑: ${if((event.damager as? Player)?.isSprinting == true) "是" else "否"}",
-                    "攻击潜行: ${if((event.damager as? Player)?.isSneaking == true) "是" else "否"}"
-                ).joinToString(" ")
-            )
+            ?.let { player ->
+                player.sendActionBar(
+                    arrayOf(
+                        "预测伤害: ${String.format("%.1f", event.damage)}",
+                        "实际伤害: ${String.format("%.1f", event.finalDamage)}",
+                        "预测距离: ${String.format("%.2f", event.damager.location.distance(event.entity.location))}",
+                        "攻击疾跑: ${if(player.isSprinting) "是" else "否"}",
+                        "攻击潜行: ${if(player.isSneaking) "是" else "否"}",
+                        "当前移速: ${ACTest.statisticsManager.getPlayerData(player).bps.getFormated(1, 2)}"
+                    ).joinToString("  ")
+                )
+            }
     }
 }
