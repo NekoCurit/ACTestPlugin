447c28d34b588414ec22ed24c22281a47f94e4c8
NekoCurit <108512490+NahidaLing@users.noreply.github.com>
2024-09-30 14:11:06 +0800

    perf(Module\DamageTest): code style

--- src/main/kotlin/kawaii/nahida/actest/module/modules/DamageTestModule.kt
+++ src/main/kotlin/kawaii/nahida/actest/module/modules/DamageTestModule.kt
@@ -13,16 +13,18 @@ class DamageTestModule : Module("DamageTest") {
     fun onPlayerAttack(event: EntityDamageByEntityEvent) {
         (event.damager as? Player)
             ?.let { player ->
-                player.sendActionBar(Component.text(
-                    arrayOf(
-                        "预测伤害: ${String.format("%.1f", event.damage)}",
-                        "实际伤害: ${String.format("%.1f", event.finalDamage)}",
-                        "预测距离: ${String.format("%.2f", event.damager.location.distance(event.entity.location))}",
-                        "攻击疾跑: ${if(player.isSprinting) "是" else "否"}",
-                        "攻击潜行: ${if(player.isSneaking) "是" else "否"}",
-                        "当前移速: ${ACTest.statisticsManager.getPlayerData(player).bps.getFormated(1, 2)}"
-                    ).joinToString("  ")
-                ))
+                player.sendActionBar(
+                    Component.text(
+                        arrayOf(
+                            "预测伤害: ${String.format("%.1f", event.damage)}",
+                            "实际伤害: ${String.format("%.1f", event.finalDamage)}",
+                            "预测距离: ${String.format("%.2f", event.damager.location.distance(event.entity.location))}",
+                            "攻击疾跑: ${if(player.isSprinting) "是" else "否"}",
+                            "攻击潜行: ${if(player.isSneaking) "是" else "否"}",
+                            "当前移速: ${ACTest.statisticsManager.getPlayerData(player).bps.getFormated(1, 2)}"
+                        ).joinToString("  ")
+                    )
+                )
             }
     }
 }
