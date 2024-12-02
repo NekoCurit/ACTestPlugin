4ddc9132234d8c30e5e910b2c18c98ab9d799d83
猫猫Curit🍀 <108512490+NekoCurit@users.noreply.github.com>
2024-12-02 16:27:37 +0800

    ci: bump kotlin 2.0.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "2.1.0-Beta1"
+    kotlin("jvm") version "2.0.21"
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
 }
